96cf7f55c14b3c7ef367de61c13a5efe3aa0cea8
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-08-08 00:01:20 +0700

    fix: validate course detail length

--- client/src/components/course/courselist-user.jsx
+++ client/src/components/course/courselist-user.jsx
@@ -113,7 +113,9 @@ function CourselistUser() {
                 {course.coursename}
               </h1>
               <p className="sm:text-black sm:text-sm font-normal">
-                {course.description}
+                {course.description.length > 85 
+                ? `${course.description.substring(0, 85)}...`
+                : course.description}
               </p>
             </div>
             <div className="w-full sm:h-[53px] border-t-[1px] border-Gray-500 text-Gray-700 text-Body3 font-Body3  flex flex-row  p-4 gap-5 ">
