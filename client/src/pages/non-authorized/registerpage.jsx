478430323dad40a75028196849f9df3e62ef718c
Krittayapornn <krittayaporn.ww@gmail.com>
2024-08-07 14:40:03 +0700

    fix: edit endpoint

--- client/src/pages/non-authorized/registerpage.jsx
+++ client/src/pages/non-authorized/registerpage.jsx
@@ -117,7 +117,7 @@ function Register() {
 
     try {
       const response = await axios.post(
-        `http://localhost:4000/users/register`,
+        `https://project-courseflow-server.vercel.app/users/register`,
         values
       );
 
