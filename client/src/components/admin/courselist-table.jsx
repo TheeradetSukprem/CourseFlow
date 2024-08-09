81ebe40d4aaacdfb35d956159567f27be009a7d8
Joom <jnwongsa@gmail.com>
2024-08-09 13:27:27 +0700

    feat: add laoding at course list

--- client/src/components/admin/courselist-table.jsx
+++ client/src/components/admin/courselist-table.jsx
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom";
 import NavbarCourseList from "./navbar/navbar-courselist";
 import { TrashIcon, PencilSquareIcon } from "@heroicons/react/24/solid";
 import ConfirmationModal from "../../components/admin/modal/delete-course-confirmation";
+import LoadingPageSvg from "../shared/loading-page";
 
 function CourseListTable() {
   const [courses, setCourses] = useState([]);
   const [search, setSearch] = useState("");
   const [openModal, setOpenModal] = useState(false);
   const [selectedCourseId, setSelectedCourseId] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const formatToBangkokTime = (isoTime) => {
     const date = new Date(isoTime);
@@ -35,7 +37,6 @@ function CourseListTable() {
 
   useEffect(() => {
     fetchCourses();
-
   }, []);
 
   const fetchCourses = async () => {
@@ -44,10 +45,12 @@ function CourseListTable() {
         "https://project-courseflow-server.vercel.app/courses"
       );
       setCourses(res.data);
-
     } catch (error) {
       console.error("Error fetching courses:", error);
+    } finally {
+      setLoading(false);
     }
+    
   };
 
   const deleteCourse = async (id) => {
@@ -79,76 +82,82 @@ function CourseListTable() {
   };
 
   return (
-    <div>
-      <NavbarCourseList search={search} onSearchChange={setSearch} />
-      <div className="m-10">
-        <table className="text-black text-sm w-full">
-          <thead className="w-[100px] h-[41px] bg-Gray-400 ">
-            <tr className="font-thin ">
-              <th className="text-left w-[48px] rounded-tl-lg"></th>
-              <th className="text-left w-[96px]">Image</th>
-              <th className="text-left w-[268px]">Course name</th>
-              <th className="text-left w-[105px]">Lesson</th>
-              <th className="text-left w-[105px]">Price</th>
-              <th className="text-left w-[188px]">Created date</th>
-              <th className="text-left w-[190px]">Updated Date</th>
-              <th className="text-left rounded-tr-lg w-[120px]">Actions</th>
-            </tr>
-          </thead>
-        </table>
-        <div className="overflow-y-scroll max-h-[550px]">
-          <table className="text-black text-sm rounded-xl w-full">
-            <tbody>
-              {courses
-                .filter((item) => {
-                  return search.trim() === ""
-                    ? true
-                    : item.coursename
-                        .toLowerCase()
-                        .includes(search.toLowerCase());
-                })
-                .map((item) => (
-                  <tr
-                    key={item.courseid}
-                    className="bg-white border-b border-Gray-400 w-[1120px] h-[88px]"
-                  >
-                    <td className="w-[48px] text-center">{item.courseid}</td>
-                    <td className="w-[96px]">
-                      <img
-                        src={item.imagefile}
-                        alt="image"
-                        style={{ width: "64px", height: "47px" }}
-                      />
-                    </td>
-                    <td className="w-[268px] text-left">{item.coursename}</td>
-                    <td className="w-[105px] text-left">
-                      {item.coursesummary} Lessons
-                    </td>
-                    <td className="w-[105px] text-left">
-                      {item.price.toFixed(2)}
-                    </td>
-                    <td className="w-[188px] text-left">
-                      {formatToBangkokTime(item.createddate)}
-                    </td>
-                    <td className="w-[190px] text-left">
-                      {formatToBangkokTime(item.updateddate)}
-                    </td>
-                    <td className="w-[120px] mt-8">
-                      <button onClick={() => handleOpenModal(item.courseid)}>
-                        <TrashIcon className="w-6 text-Blue-300 hover:text-Blue-700  text-left ml-4" />
-                      </button>
-                      <button>
-                        <Link to={`/admin/editcourse/${item.courseid}`}>
-                          <PencilSquareIcon className="w-6 text-Blue-300 hover:text-Blue-700 ml-4" />
-                        </Link>
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
+    <>
+      {loading ? (
+        <LoadingPageSvg text="Loading..." />
+      ) : (
+      <div>
+        <NavbarCourseList search={search} onSearchChange={setSearch} />
+        <div className="m-10">
+          <table className="text-black text-sm w-full">
+            <thead className="w-[100px] h-[41px] bg-Gray-400 ">
+              <tr className="font-thin ">
+                <th className="text-left w-[48px] rounded-tl-lg"></th>
+                <th className="text-left w-[96px]">Image</th>
+                <th className="text-left w-[268px]">Course name</th>
+                <th className="text-left w-[105px]">Lesson</th>
+                <th className="text-left w-[105px]">Price</th>
+                <th className="text-left w-[188px]">Created date</th>
+                <th className="text-left w-[190px]">Updated Date</th>
+                <th className="text-left rounded-tr-lg w-[120px]">Actions</th>
+              </tr>
+            </thead>
           </table>
+          <div className="overflow-y-scroll max-h-[550px]">
+            <table className="text-black text-sm rounded-xl w-full">
+              <tbody>
+                {courses
+                  .filter((item) => {
+                    return search.trim() === ""
+                      ? true
+                      : item.coursename
+                          .toLowerCase()
+                          .includes(search.toLowerCase());
+                  })
+                  .map((item) => (
+                    <tr
+                      key={item.courseid}
+                      className="bg-white border-b border-Gray-400 w-[1120px] h-[88px]"
+                    >
+                      <td className="w-[48px] text-center">{item.courseid}</td>
+                      <td className="w-[96px]">
+                        <img
+                          src={item.imagefile}
+                          alt="image"
+                          style={{ width: "64px", height: "47px" }}
+                        />
+                      </td>
+                      <td className="w-[268px] text-left">{item.coursename}</td>
+                      <td className="w-[105px] text-left">
+                        {item.coursesummary} Lessons
+                      </td>
+                      <td className="w-[105px] text-left">
+                        {item.price.toFixed(2)}
+                      </td>
+                      <td className="w-[188px] text-left">
+                        {formatToBangkokTime(item.createddate)}
+                      </td>
+                      <td className="w-[190px] text-left">
+                        {formatToBangkokTime(item.updateddate)}
+                      </td>
+                      <td className="w-[120px] mt-8">
+                        <button onClick={() => handleOpenModal(item.courseid)}>
+                          <TrashIcon className="w-6 text-Blue-300 hover:text-Blue-700  text-left ml-4" />
+                        </button>
+                        <button>
+                          <Link to={`/admin/editcourse/${item.courseid}`}>
+                            <PencilSquareIcon className="w-6 text-Blue-300 hover:text-Blue-700 ml-4" />
+                          </Link>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
+      )}
       <ConfirmationModal
         text="Are you sure you want to delete this course"
         textname="Yes, I want to delete"
@@ -156,7 +165,7 @@ function CourseListTable() {
         onClose={handleCloseModal}
         onConfirm={handleConfirmDelete}
       />
-    </div>
+    </>
   );
 }
 
